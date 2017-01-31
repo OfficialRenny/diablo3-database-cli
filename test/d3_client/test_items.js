e3ffe630cd60903ab1df16b9b83bd9bbb03d4d41
Eric Wang <skygragon@gmail.com>
2017-01-31 20:39:42 +0800

    Get item list in group.
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- test/d3_client/test_items.js
+++ test/d3_client/test_items.js
@@ -1,4 +1,5 @@
 var assert = require('chai').assert;
+var nock = require('nock');
 
 var client = require('../../lib/d3_client_en');
 
@@ -9,7 +10,10 @@ describe('d3_client_en', function() {
       client.getItemGroups(url, function(e, groups) {
         assert.equal(e, null);
         assert.equal(groups.length, 49);
-
+        assert.deepEqual(groups[0], {
+          name: 'Helms',
+          link: 'http://us.battle.net/d3/en/item/helm/',
+        });
         assert.deepEqual(groups[48], {
           name: 'Pages of Training',
           link: 'http://us.battle.net/d3/en/item/page-of-training/',
@@ -18,5 +22,27 @@ describe('d3_client_en', function() {
         done();
       });
     });
-  });
+
+    it('should get items in group', function(done) {
+      nock('http://us.battle.net')
+        .get('/d3/en/item/helm/')
+        .replyWithFile(200, './test/mock/items/helm.html.20170131');
+
+      var url = 'http://us.battle.net/d3/en/item/helm/';
+      client.getItemsInGroup(url, function(e, items) {
+        assert.equal(e, null);
+        assert.equal(items.length, 76);
+        assert.deepEqual(items[0], {
+          name: 'Helm of the Cranial Crustacean',
+          link: 'http://us.battle.net/d3/en/item/helm-of-the-cranial-crustacean',
+        });
+        assert.deepEqual(items[75], {
+          name: 'Vyr\'s Sightless Skull',
+          link: 'http://us.battle.net/d3/en/item/vyrs-sightless-skull',
+        });
+
+        done();
+      });
+    });
+  }); // #items
 });
